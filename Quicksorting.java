69f3b5e4f768f8ce1c7a3e3d93df38fbeb113882
Karlie27 <karliemmg@gmail.com>
2016-07-31 15:12:57 -0600

    Update Quicksorting.java

--- Quicksorting.java
+++ Quicksorting.java
@@ -1,33 +1,52 @@
-
-
-/* @autor Michelle
+/**
  * 
+ * @author Michelle
+ *
  */
 public class Quicksorting {
-	public static void quicksort(Comparable Array[], int izq, int der) {
-
-		  int pivote= (Integer) Array[izq];
-		  int i=izq; // busqueda izquierda-derecha
-		  int j=der; // busqueda derecha-izquierda
-		  int aux;
-		 
-		  while(i<j){            
-		     while((Integer)Array[i]<=pivote && i<j) i++; 
-		     while((Integer)Array[j]>pivote) j--;         
-		     if (i<j) {                                           
-		         aux= (Integer) Array[i];                  
-		         Array[i]=Array[j];
-		         Array[j]=aux;
-		     }
-		   }
-		  // Los números menores al pivote se ordenan a la izquierda y los mayores a él a la derecha
-		   Array[izq]=Array[j]; 
-		   Array[j]=pivote; 
-		   if(izq<j-1)
-		      quicksort(Array,izq,j-1); // Subarray izquierda se ordena
-		   if(j+1 <der)
-		      quicksort(Array,j+1,der); // Sub. derecho se ordena
+	/**
+	 * El método ordena un array de enteros comparables de izquierda a derceha
+	 * @param Array Recibe un array de números enteros
+	 * @param izq recibe los valores izq = 0
+	 * @param der recibe los valores der = número de elementos - 1
+	 */
+		public static void quicksort(Comparable Array[], int izq, int der) {
+			
+			/**
+			 * Pivote es el primer elemento del array
+			 * i realiza la búsqueda de izquierda a derecha
+			 * j hace una búsqueda de derecha a izquierda
+			 */
+			  int pivote= (Integer) Array[izq];
+			  int i=izq; // busqueda izquierda-derecha
+			  int j=der; // busqueda derecha-izquierda
+			  int aux;
+			 /**
+			  * Mientras no se hayan cruzado ambas búsquedas (i & j), siguen buscándose el elemento menor y mayor
+			  * ya si no se han cruzado aún, estos se intercambian
+			  */
+			  while(i<j){            
+			     while((Integer)Array[i]<=pivote && i<j) i++; 
+			     while((Integer)Array[j]>pivote) j--;         
+			     if (i<j) {                                           
+			         aux= (Integer) Array[i];                  
+			         Array[i]=Array[j];
+			         Array[j]=aux;
+			     }
+			   }
+			  /**
+			   * Los números menores al pivotede ordenan a la izq y los mayores a la der
+			   */
+			  
+			   Array[izq]=Array[j]; 
+			   Array[j]=pivote; 
+			   
+			   /**
+			    * Ambos subarrays (izq y der) se ordenan
+			    */
+			   if(izq<j-1)
+			      quicksort(Array,izq,j-1);
+			   if(j+1 <der)
+			      quicksort(Array,j+1,der);
+		}
 	}
-}
-
-
