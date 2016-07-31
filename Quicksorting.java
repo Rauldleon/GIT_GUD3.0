0fb97b85ad890060d2f6bebf703606212d1b8b80
Karlie27 <karliemmg@gmail.com>
2016-07-30 20:30:19 -0600

    Add files via upload

---
+++ Quicksorting.java
@@ -0,0 +1,33 @@
+
+
+/* @autor Michelle
+ * 
+ */
+public class Quicksorting {
+	public static void quicksort(int Array[], int izq, int der) {
+
+		  int pivote=Array[izq];
+		  int i=izq; // busqueda izquierda-derecha
+		  int j=der; // busqueda derecha-izquierda
+		  int aux;
+		 
+		  while(i<j){            
+		     while(Array[i]<=pivote && i<j) i++; 
+		     while(Array[j]>pivote) j--;         
+		     if (i<j) {                                           
+		         aux= Array[i];                  
+		         Array[i]=Array[j];
+		         Array[j]=aux;
+		     }
+		   }
+		  // Los números menores al pivote se ordenan a la izquierda y los mayores a él a la derecha
+		   Array[izq]=Array[j]; 
+		   Array[j]=pivote; 
+		   if(izq<j-1)
+		      quicksort(Array,izq,j-1); // Subarray izquierda se ordena
+		   if(j+1 <der)
+		      quicksort(Array,j+1,der); // Sub. derecho se ordena
+	}
+}
+
+
