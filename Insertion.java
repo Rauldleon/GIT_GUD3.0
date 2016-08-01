ab3268f2d79ffddc3e50c30d33bbac3bf153de75
Karlie27 <karliemmg@gmail.com>
2016-07-31 23:53:07 -0600

    Update Insertion.java

--- Insertion.java
+++ Insertion.java
@@ -1,20 +1,31 @@
 
-public class Insertion {
+/*
+ * Codigo obtenido en el libro JavaStructures
+ */
 
-	public static void insertionSort(int data[], int n){
-		int numSorted = 1;
-		int index;
-		while (numSorted < n){
-			int temp = data[numSorted];
-			for(index = numSorted; index > 0; index--){
-				if(temp < data[index]-1){
-					data[index]= data[index-1];
-				}else{
-					break;
-				}
-			}
-			data[index] = temp;
-			numSorted++;
-		}
-	}
+/**
+ * Clase que realiza un ordenamiento InsertionSort de una lista ingresada.
+ * @author Didier Salazar, 15487. Raul de Leon, Michelle Morales, Joselin Ortiz.
+ */
+public class InsertionSort{
+		
+    /**
+     * Metodo que realiza el ordenamiento de tipo de insercion.
+     * @param data Lista a ordenar.
+     * @return Lista ordenada.
+     * @throws Exception Por si ocurre algun error en la lista.
+     */
+    public Lista insertionSort(Lista data) throws Exception {
+      int i, j, temp;
+      for (i = 1; i < data.getLargo(); i++) {
+            temp = (Integer)data.getValor(i);
+            j = i;
+            while (j > 0 && (Integer)data.getValor(j-1) > temp) {
+                  data.editForPosition(j, data.getValor(j-1));
+                  j--;
+            }
+            data.editForPosition(j, temp);
+      }
+      return data;
+}
 }
